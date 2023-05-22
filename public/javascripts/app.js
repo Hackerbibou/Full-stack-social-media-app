ec94db967508897cc4059d104a3c78301910a357
Abiboulayed <79129877+Hackerbibou@users.noreply.github.com>
2023-05-22 08:14:54 -0400

    tr

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -6,3 +6,16 @@ $(".logo2").on('click',()=>{
     console.log('it worked')
     $(".editPopup").toggle()
 })
+$(".ap").click(function() {
+    console.log('it worked')
+    localStorage.setItem('scrollPosition', window.scrollY);
+  });
+$(".likebtn").click(function() {
+    localStorage.setItem('scrollPosition', window.scrollY);
+  });
+window.addEventListener('load', function() {
+    if(localStorage.getItem('scrollPosition') !== null){
+      window.scrollTo(0, localStorage.getItem('scrollPosition'));
+      localStorage.setItem('scrollPosition', null);
+    }
+  }, false);
